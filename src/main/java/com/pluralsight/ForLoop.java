60d8339c214904f257111ce401c6a19d77048d86
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-02 10:00:34 -0400

    done

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,9 +1,16 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
-        for(int i = 0; i <= 10; i++){
-            System.out.println(i);
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 10; i >= 0; i--){
+            if(i != 0){
+                System.out.println("Launching in " + i);
+            }
+            if (i == 0){
+                System.out.println("Launch");
+            }
+            Thread.sleep(1000);
+
         }
     }
 }
